5f5fe85313517c44a248c7240cc10e263335f36b
Chris Read <centurix@gmail.com>
2023-08-12 23:43:05 +1000

    Updated tests

--- tests/test_resource_creation.py
+++ tests/test_resource_creation.py
@@ -22,6 +22,12 @@ class FixtureTestType(BaseModel):
     description: str
 
 
+@pytest.fixture(scope="function")
+def resource(request):
+    with open(Path("tests", "fixtures", request.param), "r") as json_fixture:
+        return json.load(json_fixture)
+
+
 @pytest.fixture
 def single_resource_with_id():
     with open(
@@ -46,7 +52,16 @@ def multiple_resource_with_id():
         return json.load(json_fixture)
 
 
-def test_it_creates_a_container_from_base_model_with_id(single_resource_with_id):
+@pytest.fixture
+def multiple_resource_without_id():
+    with open(
+        Path("tests", "fixtures", "multiple_resource_without_id.json"), "r"
+    ) as json_fixture:
+        return json.load(json_fixture)
+
+
+@pytest.mark.parametrize("resource", ["single_resource_with_id.json"], indirect=True)
+def test_it_creates_a_container_from_base_model_with_id(resource):
     """
     Test that we can create a JSON:API container for a single resource
     from a pydantic BaseModel instance
@@ -59,17 +74,19 @@ def test_it_creates_a_container_from_base_model_with_id(single_resource_with_id)
     )
 
     # Create the JSON:API container
-    resource = DANJAResource.from_basemodel(basemodel_instance)
+    new_resource = DANJAResource.from_basemodel(basemodel_instance)
 
-    schema = resource.model_json_schema()
+    schema = new_resource.model_json_schema()
 
     # Check schema
-    assert(schema == single_resource_with_id)
+    assert(schema == resource)
 
     # Check resource data
-    assert(resource.data.attributes == basemodel_instance)
+    assert(new_resource.resource == basemodel_instance)
 
-def test_it_creates_a_container_from_base_model_without_id(single_resource_without_id):
+
+@pytest.mark.parametrize("resource", ["single_resource_without_id.json"], indirect=True)
+def test_it_creates_a_container_from_base_model_without_id(resource):
     """
     Test that we can create a JSON:API container for a single resource
     from a pydantic BaseModel instance
@@ -81,17 +98,19 @@ def test_it_creates_a_container_from_base_model_without_id(single_resource_witho
     )
 
     # Create the JSON:API container
-    resource = DANJAResource.from_basemodel(basemodel_instance)
+    new_resource = DANJAResource.from_basemodel(basemodel_instance)
 
-    schema = resource.model_json_schema()
+    schema = new_resource.model_json_schema()
 
     # Check schema
-    assert(schema == single_resource_without_id)
+    assert(schema == resource)
 
     # Check resource data
-    assert(resource.data.attributes == basemodel_instance)
+    assert(new_resource.resource == basemodel_instance)
 
-def test_it_creates_a_container_list_from_base_model_with_id(multiple_resource_with_id):
+
+@pytest.mark.parametrize("resource", ["multiple_resource_with_id.json"], indirect=True)
+def test_it_creates_a_container_list_from_base_model_with_id(resource):
     """
     Test that we can create a JSON:API container for a single resource
     from a pydantic BaseModel instance
@@ -111,12 +130,42 @@ def test_it_creates_a_container_list_from_base_model_with_id(multiple_resource_w
     ]
 
     # Create the JSON:API container
-    resource = DANJAResourceList.from_basemodel_list(basemodel_instances)
+    new_resources = DANJAResourceList.from_basemodel_list(basemodel_instances)
 
-    # schema = resource.model_json_schema()
+    schema = new_resources.model_json_schema()
 
     # Check schema
-    # assert(schema == multiple_resource_with_id)
+    assert(schema == resource)
 
     # Check resource data
-    assert(len(resource.data) == len(basemodel_instances))
+    assert(len(new_resources.resources) == len(basemodel_instances))
+
+
+@pytest.mark.parametrize("resource", ["multiple_resource_without_id.json"], indirect=True)
+def test_it_creates_a_container_list_from_base_model_without_id(resource):
+    """
+    Test that we can create a JSON:API container for a single resource
+    from a pydantic BaseModel instance
+    """
+    # BaseModel including ID
+    basemodel_instances = [
+        FixtureTestType(
+            name="Stuff!",
+            description="This is desc!"
+        ),
+        FixtureTestType(
+            name="More Stuff!",
+            description="This is more desc!"
+        )
+    ]
+
+    # Create the JSON:API container
+    new_resources = DANJAResourceList.from_basemodel_list(basemodel_instances)
+
+    schema = new_resources.model_json_schema()
+
+    # Check schema
+    assert(schema == resource)
+
+    # Check resource data
+    assert(len(new_resources.resources) == len(basemodel_instances))
