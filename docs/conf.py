477fb5749b98b7bd15fc24484cdc42e7ceb1c986
Chris Read <centurix@gmail.com>
2023-09-23 17:25:06 +1000

    Updated types for Sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+from typing import List
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,23 +7,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PyDANJA'
-copyright = '2023, Chris Read'
-author = 'Chris Read'
-release = '0.1.17'
+project: str = 'PyDANJA'
+copyright: str = '2023, Chris Read'
+author: str = 'Chris Read'
+release: str = '0.1.17'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions: List[str] = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path: List[str] = ['_templates']
+exclude_patterns: List[str] = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme: str = 'alabaster'
+html_static_path: List[str] = ['_static']
